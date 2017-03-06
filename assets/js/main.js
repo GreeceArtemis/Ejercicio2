3fcf58a15f45e74160afce855668b3adadceee0f
GreeceArtemis <greece.rapu@gmail.com>
2017-03-05 23:39:50 -0500

    paginas

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,8 @@
-function convertirCelsious(){
 var f=document.getElementById("fahrenheit");
+var c=document.getElementById("celsious");
+function convertirCelsious(){
 document.getElementById("result").innerHTML=((f.value-32)*(5/9)).toFixed(2)+" C";
 }
 function convertirFahrenheit(){
-  var c=document.getElementById("celsious");
-
   document.getElementById("result").innerHTML=(c.value*(9/5)+32).toFixed(2)+" F";
 }
